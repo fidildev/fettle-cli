b283a474944565cf0ecd3bd6f597c4cda18c33e5
Tyler Kron <tylerkron@gmail.com>
2023-03-12 19:31:57 -0600

    feat: use fettle config by default (#43)
    * added logic to use the fettle config. Falls back to the environment variables if config values don't exist
    
    * Added companion object to the GitHubConfig data class
    
    * updates from PR review
    
    * fixed bug. need to check the existence of the config file, not the directory

--- src/main/kotlin/dev/fidil/fettle/FettleApplication.kt
+++ src/main/kotlin/dev/fidil/fettle/FettleApplication.kt
@@ -4,24 +4,73 @@ package dev.fidil.fettle
 
 import dev.fidil.fettle.command.BranchProtectionCommand
 import dev.fidil.fettle.command.DependabotCommand
+import dev.fidil.fettle.config.GitHubConfig
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ExperimentalCli
+import org.yaml.snakeyaml.Yaml
+import java.nio.file.Files
 import java.util.*
 import kotlin.system.exitProcess
 
 
 fun main(args: Array<String>) {
-    val ghToken = System.getenv("GH_TOKEN")
-    val ghUser = System.getenv("GH_USER")
-    if (ghToken == null || ghUser == null) {
-        println("A valid GitHub user and token are required to be in your environment. Example \nexport GH_TOKEN=sometoken\nexport GH_USER=myuser")
-        exitProcess(1)
-    }
+    createFettleConfigDirIfNotExists()
+    checkFettleConfigPermissions()
+    val (ghUser, ghToken) = getGitHubConfig()
 
     val parser = ArgParser("fettle-cli")
     parser.subcommands(BranchProtectionCommand(ghUser, ghToken), DependabotCommand(ghUser, ghToken))
     parser.parse(args)
 
     println("╭ᥥ╮(´• ᴗ •`˵)╭ᥥ╮")
+}
+fun createFettleConfigDirIfNotExists() {
+    if (!GitHubConfig.fettleDir.exists()) {
+        if (GitHubConfig.fettleDir.mkdir()) {
+        } else {
+            println("Warning: Failed to create the fettle config directory.")
+        }
+    }
+}
 
+fun checkFettleConfigPermissions() {
+    if (!GitHubConfig.configFile.exists()) {
+        return
+    }
+    val actualPermission = Files.getPosixFilePermissions(GitHubConfig.configFile.toPath())
+    if (actualPermission != GitHubConfig.requiredFilePermission) {
+        println("Fettle config has the wrong permission. Please set it to 600 for security reasons.")
+        exitProcess(1)
+    }
+}
+
+fun getGitHubConfig(): Pair<String, String> {
+    var ghUser: String? = null
+    var ghToken: String? = null
+
+    if (GitHubConfig.configFile.exists()) {
+        val yaml = Yaml()
+        val configData = yaml.loadAs(GitHubConfig.configFile.inputStream(), GitHubConfig::class.java)
+
+        if (configData != null) {
+            val githubConfig = configData.github
+            ghUser = githubConfig.user
+            ghToken = githubConfig.token
+        }
+    }
+
+    if (ghUser == null) {
+        ghUser = System.getenv("GH_USER")
+    }
+
+    if (ghToken == null) {
+        ghToken = System.getenv("GH_TOKEN")
+    }
+
+    if (ghToken == null || ghUser == null) {
+        println("A valid GitHub user and token are required to be in your fettle config or in your environment. Environment Example: \nexport GH_TOKEN=sometoken\nexport GH_USER=myuser")
+        exitProcess(1)
+    }
+
+    return Pair(ghUser, ghToken)
 }
