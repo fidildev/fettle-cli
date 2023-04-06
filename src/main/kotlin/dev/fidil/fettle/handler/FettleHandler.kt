4588291be698f66398ea2a00912a21859a54ecbd
mikey <mikey@fidil.dev>
2023-04-05 21:07:03 -0600

    Spike in something

--- src/main/kotlin/dev/fidil/fettle/handler/FettleHandler.kt
+++ src/main/kotlin/dev/fidil/fettle/handler/FettleHandler.kt
@@ -2,10 +2,41 @@ package dev.fidil.fettle.handler
 
 import dev.fidil.fettle.command.CommandResult
 import dev.fidil.fettle.command.FettleContext
+import org.reflections.Reflections
+import java.lang.reflect.Modifier
 
 interface FettleHandler {
 
     val context: FettleContext
+
+    object CommandFactory {
+        inline fun <reified T> getImplementations(): List<Class<out T>> {
+            val classes = mutableListOf<Class<out T>>()
+            val reflections = Reflections("dev.fidil.fettle.handler")
+            val subTypes = reflections.getSubTypesOf(T::class.java)
+            for (subType in subTypes) {
+                if (Modifier.isAbstract(subType.modifiers)) {
+                    continue
+                }
+                classes.add(subType as Class<out T>)
+            }
+            return classes
+        }
+    }
+
+    fun findFettleFunctions(): Map<String, FettleHandler> {
+        val scoreMethodMap = HashMap<String, FettleHandler>()
+        for (implementation in dev.fidil.fettle.CommandFactory.getImplementations<FettleHandler>()) {
+            if (implementation.simpleName == "score") {
+                continue
+            } else {
+                scoreMethodMap[implementation.simpleName] =
+                    implementation.getDeclaredConstructor(FettleHandler::class.java).newInstance()
+            }
+        }
+
+        return scoreMethodMap
+    }
 
     fun branchProtections(org: String, repo: String, branch: String): CommandResult
 
