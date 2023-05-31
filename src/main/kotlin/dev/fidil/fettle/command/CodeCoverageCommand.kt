58c63e89d529d92c1b7f2a41d4c8fb4cddd1fac9
Tyler Kron <tylerkron@gmail.com>
2023-05-30 20:56:07 -0600

    docs: added missing readme info (#90)

--- src/main/kotlin/dev/fidil/fettle/command/CodeCoverageCommand.kt
+++ src/main/kotlin/dev/fidil/fettle/command/CodeCoverageCommand.kt
@@ -3,7 +3,7 @@ package dev.fidil.fettle.command
 import dev.fidil.fettle.handler.FettleHandler
 
 class CodeCoverageCommand(override val handler: FettleHandler) :
-    GitHubRepoSubCommand("codeCoverage", "Validate Code Coverage is Calculated") {
+    GitHubRepoSubCommand("codeCoverage", "Validate Code Coverage is calculated") {
 
     override fun processCommand(): CommandResult {
         return handler.codeCoverage(org, repo, branch)
